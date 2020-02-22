1b177dc76a380a1ca5037771184b5000b45b48e8
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2020-02-22 16:08:00 -0500

    v1 complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,21 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <div className="nav-container">
-      <nav className="nes-container is-dark is-rounded">
-        <Link to="/Survivor" className="nes-text is-warning">
-          Survivor
-        </Link>
-        <Link to="/Killer" className="nes-text is-warning">
-          Killer
-        </Link>
-      </nav>
+      <div className="nes-container is-dark is-rounded home-container">
+        <nav>
+          <Link to="/Survivor" className="nes-text is-warning">
+            Survivor
+          </Link>
+          <Link to="/Killer" className="nes-text is-warning">
+            Killer
+          </Link>
+        </nav>
+        <img
+          className="home-img"
+          src={require("../assets/deathslinger.jpeg")}
+          alt="deathslinger wallpaper"
+        />
+      </div>
     </div>
   );
 };
